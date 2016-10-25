1e4e6b66d840e6e3cd0a35d385ce1d15dafe3be1
Amir Ayub <dev.amirayub@gmail.com>
2016-10-25 14:07:12 +0400

    Using chronometer

--- app/src/main/java/com/aaxs/nitrov17/simpletimer/MainActivity.java
+++ app/src/main/java/com/aaxs/nitrov17/simpletimer/MainActivity.java
@@ -2,12 +2,52 @@ package com.aaxs.nitrov17.simpletimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    Chronometer chronometer;
+    Button button;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar =(SeekBar) findViewById(R.id.seekBar);
+        chronometer=(Chronometer) findViewById(R.id.chronometer2);
+        button = (Button) findViewById(R.id.button);
+
+        seekBar.setMax(600); //Max in seconds
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                updateChronometer(i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    void updateChronometer(int a)
+    {
+        int val = a*1000;
+        chronometer.setCountDown(true);
+        
+    }
+
 }
