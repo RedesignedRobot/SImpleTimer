7bf5d462c7addaf433d4a0222a592c5c52ce50a8
Amir Ayub <dev.amirayub@gmail.com>
2016-10-27 00:14:00 +0400

    Core Functionality Completed

--- app/src/main/java/com/aaxs/nitrov17/simpletimer/MainActivity.java
+++ app/src/main/java/com/aaxs/nitrov17/simpletimer/MainActivity.java
@@ -1,23 +1,138 @@
 package com.aaxs.nitrov17.simpletimer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     SeekBar seekBar;
-    Chronometer chronometer;
+    TextView textView;
     Button button;
-    int i;
+    CountDownTimer countDownTimer;
+    int x;
+    boolean isTimerActive=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar=(SeekBar)findViewById(R.id.seekBar);
+        textView=(TextView)findViewById(R.id.textView);
+        button=(Button)findViewById(R.id.button);
+
+        initView();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                x=i;
+                setPreTimerText((long)x);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isTimerActive==false) {
+                    primeTimer(x);
+                    isTimerActive = true;
+                    countDownTimer.start();
+                    seekBarEnabler();
+                }
+                else if(isTimerActive)
+                {
+                    timerReset();
+                }
+            }
+        });
+
+
     }
+
+    void initView()
+    {
+        seekBar.setMax(600);    //In Seconds
+        seekBar.setProgress(0);
+        textView.setText("0");
+        button.setText("Start");
+    }
+
+    void setPreTimerText(long a)
+    {
+        textView.setText(String.valueOf(a));
+    }
+
+    void updateText(long a)
+    {
+        int val = (int)a/1000;
+        textView.setText(String.valueOf(val));
+    }
+
+    void primeTimer(int a)
+    {
+        int val=a*1000;
+        countDownTimer= new CountDownTimer(val,1000) {
+            @Override
+            public void onTick(long l) {
+
+                updateText(l);
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                textView.setText("Times Up!");
+                isTimerActive=false;
+                seekBarEnabler();
+
+            }
+        };
+    }
+
+    void seekBarEnabler()
+    {
+        if(isTimerActive)
+        {
+            seekBar.setEnabled(false);
+            button.setText("STOP");
+        }
+
+        else
+        {
+            seekBar.setEnabled(true);
+            button.setText("START");
+        }
+    }
+
+    void timerReset()
+    {
+        countDownTimer.cancel();
+        textView.setText("Stopped");
+        isTimerActive=false;
+        seekBarEnabler();
+        seekBar.setProgress(0);
+    }
+
+
+
 
 
 }
