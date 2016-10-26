0cd811936961a705710bd6d7b0a7ed75262b3dcb
Amir Ayub <dev.amirayub@gmail.com>
2016-10-26 22:45:38 +0400

    Primary Commit

--- app/src/main/java/com/aaxs/nitrov17/simpletimer/MainActivity.java
+++ app/src/main/java/com/aaxs/nitrov17/simpletimer/MainActivity.java
@@ -2,12 +2,22 @@ package com.aaxs.nitrov17.simpletimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    Chronometer chronometer;
+    Button button;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
